a9b3ca7890fb0cd034e6d98dbf6e64eb18df5933
unknown <luckyrabbit870930@gmail.com>
2021-01-14 01:23:20 +0900

    .

--- Client.js
+++ Client.js
@@ -23,7 +23,7 @@ async function retrieveLatestEthPrice () {
 async function filterEvents (callerContract) {
   callerContract.events.PriceUpdatedEvent({ filter: { } }, async (err, event) => {
     if (err) console.error('Error on event', err);
-    console.log('* New PriceUpdated event. ethPrice: ' + event.returnValues.ethPrice)
+    console.log('* New PriceUpdated event. ethPrice: ' + event.returnValues.ethPrice);
   })
   callerContract.events.ReceivedNewRequestIdEvent({ filter: { } }, async (err, event) => {
     if (err) console.error('Error on event', err);
